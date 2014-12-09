376912df858280b8991e16f95740784e03448055
Ivan Ermilov <earthquakesan@gmail.com>
2014-12-09 16:00:19 +0100

    basic package dumping for pdeu and datagov

--- ckanaggregatorpy/datagov/package_cache.py
+++ ckanaggregatorpy/datagov/package_cache.py
@@ -1,18 +1,23 @@
 import os.path
 import time
+import requests
 
 try:
     import cPickle as pickle
 except:
     import pickle
 
-import ckanaggregatorpy.pdeu as pdeu
+import ckanaggregatorpy.datagov as datagov
 
 class PackageCache(object):
-    packageListFile = os.path.join(pdeu.cacheFolder, "packageList.dump")
-    packagesFolder = os.path.join(pdeu.cacheFolder, "packages")
+    packageListFile = os.path.join(datagov.cacheFolder, "packageList.dump")
+    packagesFolder = os.path.join(datagov.cacheFolder, "packages")
+    pagesFolder = os.path.join(datagov.cacheFolder, "pages")
+    datagovPageUrl = "http://catalog.data.gov/dataset?page="
+    datagovNumberOfPages = 6705 # this number has to be updated manually
+
     def __init__(self):
-        self.ckanClient = pdeu.ckanClient
+        self.ckanClient = datagov.ckanClient
 
     def getPackages(self):
         pass
@@ -56,8 +61,51 @@ class PackageCache(object):
         return obj
 
     def updatePackageList(self):
-        packageList = self.ckanClient.package_list()
-        self.saveFile(self.packageListFile, packageList)
+        """
+            ckanclient.package_list() does not work for data.gov portal
+            we have to fetch all the pages from the web portal 
+            and then scrape package ids from them
+        """
+        self.updateDataGovPages()
+        self.extractDataGovDatasetIds()
+
+    def extractDataGovDatasetIds(self):
+        """
+            Extract Dataset Ids from the scraped pages
+            Should be run after updateDataGovPages
+        """
+        datasetIds = []
+        for i in range(1, self.datagovNumberOfPages + 1):
+            pageFile = os.path.join(self.pagesFolder, "page" + str(i))
+            pageUrl = self.datagovPageUrl+str(i)
+            if(not os.path.isfile(pageFile)):
+                print("Page %s does not exist! Did you run updateDataGovPages?" % pageUrl)
+                break
+            f = open(pageFile, "rU")
+            page = f.read()
+            f.close()
+            soup = BeautifulSoup(page)
+            for dataset in soup.find_all(href=re.compile("dataset/")):
+                datasetIds.append(dataset["href"].split("/")[-1])
+         
+        self.saveFile(packageListFile, datasetIds)
+            
+    def updateDataGovPages(self):
+        """
+            Saves all the pages from the data.gov portal to data/datagov/pages folder
+        """
+        for i in range(1, self.datagovNumberOfPages + 1):
+            print("Getting page %s out of %s" % (i, self.datagovNumberOfPages))
+            pageFile = os.path.join(self.pagesFolder, "page" + str(i))
+            pageUrl = self.datagovPageUrl+str(i)
+            if(os.path.isfile(pageFile)):
+                print("Page %s is already fetched, skipping." % pageUrl)
+            r = requests.get(pageUrl)
+            assert r.status_code == requests.status_codes.codes.OK
+            f = open(pageFile, "w")
+            f.write(r.content)
+            f.close()
+            time.sleep(0.5)
 
     def isPackageListOutdated(self):
         #Does not exist
@@ -82,6 +130,6 @@ class PackageCache(object):
 if __name__ == "__main__":
     packageCache = PackageCache()
     pkgList = packageCache.getPackageList()
-    packageCache.updatePackages()
+    #packageCache.updatePackages()
     import ipdb; ipdb.set_trace()
     print "hi"
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir data/pdeu
+mkdir data/pdeu/packages
+mkdir data/datagov
+mkdir data/datagov/pages
